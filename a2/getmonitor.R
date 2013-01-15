a85289857e068f4355688f23622b5a1868d4febf
Jeremy Hageman <jjhageman@gmail.com>
2013-01-15 00:31:51 -0800

    part 1

--- a2/getmonitor.R
+++ a2/getmonitor.R
@@ -1,11 +1,8 @@
 getmonitor <- function(id, directory, summarize = FALSE) {
-  file = paste0(directory,'/',sprintf("%03d",id),'.csv')
+  file = paste0(directory,'/',sprintf("%03d",as.numeric(id)),'.csv')
   data <- read.csv(file,header = TRUE)
   if(summarize) {
-    dates <- data[1:6]['Date']
-    other_dates = nrow(data)-6
-    print(data)
-  }else{
-    data
+    print(summary(data))
   }
+  data
 }
